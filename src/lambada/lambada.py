6b66bbed656121f6602da1e49caa96fcde9e3434
sT0v <research.stokes@gmail.com>
2024-05-18 21:34:15 -0500

    abstracted the animate method to load different emote artifacts

--- src/lambada/lambada.py
+++ src/lambada/lambada.py
@@ -1,44 +1,74 @@
+import re
 import os
-import time
 import sys
+import time
+import click
+from pathlib import Path
+try:
+    from typing import Literal, List
+except:
+    from typing_extensions import Literal, List
 
-FRAME1 = """ 
-                           //
-( ** )                    //
-  \/ ====================/
-  @@                    @@ 
-"""
 
-FRAME2 = r"""
-                        \\
-( ** )                   \\
-  \/ ====================/
-  @@                    @@ 
-"""
-
-# Function to clear the screen
 def clear_screen():
-    # Windows
+    # Windows <-----------------------> macOS and Linux
     os.system('cls') if os.name == 'nt' else os.system('clear')
         
-    # macOS and Linux
-        
 
-def lambada():
+def load_frame(path: str) -> str:
+    with open(path, "r", encoding="utf-8") as f:
+        frame = f.read()
+    return frame
+
+
+def sorted_alphanumeric(data):
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
+
+
+def load_animation(animation: Literal["wag", "poo"]) -> List[Path]:
+    cwd = os.getcwd()
+    animation_artifacts = {
+        "wag": os.path.join(cwd, Path("src/artifacts/animation/wag")),
+        "poo": os.path.join(cwd, Path("src/artifacts/animation/poo")),
+    }
+
+    frame_artifact_files = [
+        os.path.join(animation_artifacts[animation], frame_file) 
+        for frame_file in os.listdir(animation_artifacts[animation])
+    ]
+
+    # print(sorted_alphanumeric(frame_artifact_files))
+    frame_artifact = [load_frame(path) for path in sorted_alphanumeric(frame_artifact_files)]
+    return frame_artifact    
+
+
+def animate(animation_artifacts: List[str], speed: float) -> None:
+    for frame in animation_artifacts:
+        clear_screen()
+        print(frame)
+        time.sleep(speed)
+
+
+@click.command()
+@click.option("-a", "--animation", default="wag")
+@click.option("-s", "--speed", default=.5)
+def lambada(animation: Literal["wag", "poo"], speed: float = .5):
+    
+    supported_animations = ["wag", "poo"]
+    if animation not in supported_animations:
+        raise ValueError(
+            "Animation {animation} is not supported.".format(animation=animation) +
+            "Choose from {supported_animations}".format(supported_animations=", ".join(supported_animations))
+        )
+    
+    frames = load_animation(animation)
     try:
         while True:
-            # Clear screen and print _s1
-            clear_screen()
-            print(FRAME1)
-            time.sleep(0.5)  # Adjust the speed as needed
+            animate(animation_artifacts=frames, speed=speed)
             
-            # Clear screen and print _s2
-            clear_screen()
-            print(FRAME2)
-            time.sleep(0.5)  # Adjust the speed as needed
     except KeyboardInterrupt:
-        # Optionally clear the screen one last time when exiting
-        # clear_screen()
         print("Animation stopped.")
         sys.exit()
 
